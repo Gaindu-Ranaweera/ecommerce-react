30595ef36e94bf68d2becbbe3386ee39862abb26
Gaindu Ranaweera <gaindukithmal.a@gmail.com>
2025-10-19 10:29:27 +0530

    deploy error 1

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,20 +1,17 @@
-import { clerkClient } from "@clerk/nextjs/server"
-
-
+import { clerkClient } from "@clerk/nextjs/server";
 
 const authAdmin = async (userId) => {
-  try{
-    if (!userId) return false
-    
-    const client = await clerkClient()
-    const user = await client.users.getUser(userId)
+  try {
+    if (!userId) return false;
 
-    return process.env.ADMIN_EMAIL.split(',').includes(user.emailAddresses[0].emailAddress)
-
-  }catch (error){
-    console.error(error)
-    return false
+    const user = await clerkClient.users.getUser(userId);
+    return process.env.ADMIN_EMAIL.split(",").includes(
+      user.emailAddresses[0].emailAddress
+    );
+  } catch (error) {
+    console.error(error);
+    return false;
   }
-}
+};
 
-export default authAdmin
+export default authAdmin;
